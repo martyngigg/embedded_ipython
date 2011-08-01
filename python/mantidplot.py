c49c71a2219e21ba1d63cc0d934a3e5e7e97e00b
Martyn Gigg <martyn.gigg@gmail.com>
2011-08-01 17:27:43 +0100

    Add basic layout and CMake files

---
+++ python/mantidplot.py
@@ -0,0 +1,28 @@
+import sys
+
+from PyQt4.QtGui import QApplication
+from mantidplotpy import MainWindow
+#from ipythonshell import PythonShell
+from ipython_view import IPythonView
+
+
+def qapplication():
+    """
+    Return QApplication instance
+    Creates it if it doesn't already exist
+    """
+    app = QApplication.instance()
+    if not app:
+        app = QApplication([])
+    return app
+
+def main():
+    app = qapplication()
+    mainwin = MainWindow()
+    shell = IPythonView()
+    mainwin.insertPythonShell(shell)
+    mainwin.show()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
