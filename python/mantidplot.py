3f7c593b9707a4ad796faaa693f9ed66a0cb8b9d
Martyn Gigg <martyn.gigg@gmail.com>
2011-08-05 15:36:38 +0100

    Code now uses built-in IPython Qt widget from 0.11

--- python/mantidplot.py
+++ python/mantidplot.py
@@ -1,8 +1,14 @@
 import sys
+import signal
 
-from PyQt4.QtGui import QApplication
+#from PyQt4.QtGui import QApplication
+#from widgets.ipython_view import IPythonView
+
+from IPython.external.qt import QtGui
+from IPython.frontend.qt.kernelmanager import QtKernelManager
+from IPython.frontend.qt.console.rich_ipython_widget import RichIPythonWidget
+
 from mantidplotlib import MainWindow
-from widgets.ipython_view import IPythonView
 
 
 def qapplication():
@@ -10,18 +16,43 @@ def qapplication():
     Return QApplication instance
     Creates it if it doesn't already exist
     """
-    app = QApplication.instance()
+    app = QtGui.QApplication.instance()
     if not app:
-        app = QApplication([])
+        app = QtGui.QApplication([])
     return app
 
+def init_kernel_manager():
+    # Don't let Qt or ZMQ swallow KeyboardInterupts.
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+
+    ip = '127.0.0.1'
+
+    # Create a KernelManager and start a kernel.
+    kernel_manager = QtKernelManager(
+                            shell_address=(ip, 0),
+                            sub_address=(ip, 0),
+                            stdin_address=(ip, 0),
+                            hb_address=(ip, 0)
+                            #,
+                            #config=self.config
+    )
+    # start the kernel
+    kwargs = dict(ip='127.0.0.1', ipython=True)
+    #kwargs['extra_arguments'] = self.kernel_argv
+    kernel_manager.start_kernel(**kwargs)
+    kernel_manager.start_channels()
+    return kernel_manager
+    
 def main():
     app = qapplication()
     mainwin = MainWindow()
-    shell = IPythonView()
+
+    shell = RichIPythonWidget(local_kernel=False)
+    shell.kernel_manager = init_kernel_manager()
     mainwin.insertPythonShell(shell)
+    
     mainwin.show()
-    sys.exit(app.exec_())
+    app.exec_()
 
 if __name__ == '__main__':
     main()
